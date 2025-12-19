2754afb1521932e93ec650bcd4de843c84a25e51
123nghia <nghiait06@gmail.com>
2025-12-19 14:01:43 +0700

    Serve /api/docs via RestController redirect

--- p2p_p2p/src/main/java/com/akabazan/api/controller/DocsController.java
+++ p2p_p2p/src/main/java/com/akabazan/api/controller/DocsController.java
@@ -1,13 +1,20 @@
 package com.akabazan.api.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import java.net.URI;
 
-@Controller
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class DocsController {
 
     @GetMapping({"/docs", "/docs/"})
-    public String docs() {
-        return "redirect:/swagger-ui/index.html";
+    public ResponseEntity<Void> docs() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(URI.create("/swagger-ui/index.html"));
+        return new ResponseEntity<>(headers, HttpStatus.FOUND);
     }
 }
