da4995396d7ab1fcf8d3637d37354624735fa605
123nghia <nghiait06@gmail.com>
2025-12-23 08:47:06 +0700

    fix code

--- p2p_admin/src/main/java/com/akabazan/admin/AdminApplication.java
+++ p2p_admin/src/main/java/com/akabazan/admin/AdminApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import java.util.TimeZone;
 
 @SpringBootApplication(scanBasePackages = {
         "com.akabazan.admin",
@@ -24,6 +25,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 })
 public class AdminApplication {
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
         SpringApplication.run(AdminApplication.class, args);
     }
 }
